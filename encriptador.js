6b5dd8b8142e93e37707c19a7c486408b8f49566
ElPanaMael <miguelmonroy@live.com>
2024-02-28 14:37:23 -0500

    Back completamente funcional

--- encriptador.js
+++ encriptador.js
@@ -45,10 +45,29 @@ function encripta(texto){
 }
 
 function desencripta(texto){
+    texto = texto.split(" ");
+    for(let i = 0; i<texto.length; i++){
+        if (texto[i].includes('enter')||texto[i].includes('ai')||texto[i].includes('imes')
+            ||texto[i].includes('ober') || texto[i].includes('ufat')) {
+            texto[i] = change(texto[i]) + ' ';
+        }
+    console.log(texto)
+    mostrarTexto(texto);
+    }
+}
 
+function change(texto){
+    console.log(texto)
+    texto = texto.replace(/ai/gi, 'a');
+    texto = texto.replace(/enter/gi, 'e');
+    texto = texto.replace( /imes/gi, "i" );
+    texto = texto.replace(/ober/gi, 'o');
+    texto = texto.replace(/ufat/gi, 'u');
+    return(texto);
 }
 
 function mostrarTexto(texto){
+    console.log(texto)
     texto = texto.join("");
     let img = document.getElementById('muneco');
     img.setAttribute('hidden', 'hidden');
